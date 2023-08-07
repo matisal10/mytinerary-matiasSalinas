1c78ade993e71ba5016c62e9e6c0d980498ce032
MatiSal <salinasmatias100@gmail.com>
2023-08-07 18:25:51 -0300

    change hero style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,32 +3,30 @@ import '../App.css'
 import { Button, Image } from '@chakra-ui/react'
 import Navbar from './Navbar/Navbar';
 import Carousel from './Carousel/Carousel';
-import img1 from '/assets/city1.png';
-import img2 from '/assets/city2.png';
-import img3 from '/assets/city3.png';
-import img4 from '/assets/city4.png';
-
-const images = [img1, img2, img3,img4];
-
 
 function Home() {
-
 
     return (
         <>
             <main>
-                <section className="hero">
-                    <div className="content">
-                        <h1>Find the perfect destination</h1>
-                        <p>Our app will help you find the perfect path for your next trip.
-                            With an easy-to-use interface and a host of itinerary options, planning your next trip has never been easier.</p>
-                        <Button colorScheme='facebook'>View More</Button>
+                <section id="home-section" className="hero">
+                    <div className="hero-background">
+                        {/* Content inside the hero section */}
+                        <div className="content">
+                            <h1 style={{color:'#ffbc40'}}>Find the perfect destination</h1>
+                            <p>
+                                Our app will help you find the perfect path for your next trip. With an easy-to-use
+                                interface and a host of itinerary options, planning your next trip has never been
+                                easier.
+                            </p>
+                            <Button colorScheme="facebook" width="300px">
+                                View More
+                            </Button>
+                        </div>
                     </div>
-
-                    <Image src="gibbresh.png" fallbackSrc="https://via.placeholder.com/200" />
                 </section>
                 <section>
-                    <Carousel images={images} />
+                    <Carousel />
                 </section>
             </main>
 
