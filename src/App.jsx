20fb00f6a3942ba0218043f52e42b9f91061e1cc
MatiSal <salinasmatias100@gmail.com>
2023-08-07 18:26:07 -0300

    change style navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { AppRoute } from './Routes/route';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
+  const [navbarColor, setNavbarColor] = useState('transparent');
+  const [logoNavColor, setLogoNavColor] = useState('white');
+
+  const handleScroll = () => {
+    const homeSection = document.getElementById('home-section');
+
+    if (homeSection) {
+      const scrollPosition = window.scrollY;
+      const homeSectionHeight = homeSection.offsetHeight;
+      const triggerPoint = 50; 
+
+      if (scrollPosition > triggerPoint) {
+        setNavbarColor('#FFF'); 
+        setLogoNavColor('black')
+      } else {
+        setNavbarColor('transparent'); 
+        setLogoNavColor('white')
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <>
       <header>
-        <Navbar />
+        <Navbar backgroundColor={navbarColor} logoColor={logoNavColor} />
       </header>
       <AppRoute />
       <footer>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import {
     BrowserRouter as Router,
         Link,
 } from "react-router-dom";
-import { Button,Icon } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import './Navbar.css'
 
-const Navbar = () => {
+const Navbar = ({ backgroundColor, logoColor }) => {
     return (
-        <div>
-            <nav className="navbar">
-                <h1 className="logo">My Tinerary</h1>
+        <div style={{ backgroundColor }} className="navbar">
+            <nav className='nav'>
+                <h1 style={{ color:logoColor }} className="logo">My Tinerary</h1>
                 <ul className="nav-links">
                     <li><Link to="/ ">Home</Link></li>
                     <li><Link to="/cities" style={{ padding: "0px 1rem" }}>Cities</Link></li>
